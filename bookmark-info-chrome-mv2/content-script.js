833931ff32d108d150611e0156c19b0856effffe
PavelPoroskov <pavel.poroskov.se@gmail.com>
2024-02-13 07:31:28 +0300

    z-index for developer.chrome.com

--- bookmark-info-chrome-mv2/content-script.js
+++ bookmark-info-chrome-mv2/content-script.js
@@ -22,7 +22,7 @@ const log = SHOW_LOG ? console.log : () => {};
     'position: fixed',
     'right: 0',
     'top: 0',
-    'z-index: 999',
+    'z-index: 9999',
     'display: flex',
     'background-color: transparent',
   ].join(';');
