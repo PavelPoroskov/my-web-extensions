00409bc34033207c0e7d484b7c943348e2efab98
PavelPoroskov <pavel.poroskov.se@gmail.com>
2024-05-17 23:44:24 +0200

    fix errata

--- bookmark-info-chrome-mv3/api/link-api.js
+++ bookmark-info-chrome-mv3/api/link-api.js
@@ -3,6 +3,8 @@ const exceptionList = [
   'youtube.com',
   'www.youtube.com',
   'youtu.be',
+  'career.proxify.io',
+  'proxify.io',
 ]
 const exceptionSet = new Set(exceptionList)
 
@@ -23,16 +25,16 @@ export const cleanLink = (link) => {
   }
 }
 
-let testStr = "https://www.linkedin.com/jobs/view/3920634940/?alternateChannel=search&refId=dvaqme%2FfxHehSAa5o4nVnA%3D%3D&trackingId=8%2FZKaGcTAInuTTH4NyKDoA%3D%3D"
-console.log('test cleanLink', cleanLink(testStr))
+// let testStr = "https://www.linkedin.com/jobs/view/3920634940/?alternateChannel=search&refId=dvaqme%2FfxHehSAa5o4nVnA%3D%3D&trackingId=8%2FZKaGcTAInuTTH4NyKDoA%3D%3D"
+// console.log('test cleanLink', cleanLink(testStr))
 
-testStr = "https://www.youtube.com/watch?v=YuJ6SasIS_E&t=356s"
-console.log('test cleanLink', cleanLink(testStr))
+// testStr = "https://www.youtube.com/watch?v=YuJ6SasIS_E&t=356s"
+// console.log('test cleanLink', cleanLink(testStr))
 
-testStr = "https://youtube.com/watch?v=YuJ6SasIS_E&t=356s"
-console.log('test cleanLink', cleanLink(testStr))
+// testStr = "https://youtube.com/watch?v=YuJ6SasIS_E&t=356s"
+// console.log('test cleanLink', cleanLink(testStr))
 
-testStr = "https://youtu.be/watch?v=YuJ6SasIS_E&t=356s"
-console.log('test cleanLink', cleanLink(testStr))
+// testStr = "https://youtu.be/watch?v=YuJ6SasIS_E&t=356s"
+// console.log('test cleanLink', cleanLink(testStr))
 //
 // https://career.proxify.io/apply?uuid=566c933b-432e-64e0-b317-dd4390d6a74e&step=AdditionalInformation
