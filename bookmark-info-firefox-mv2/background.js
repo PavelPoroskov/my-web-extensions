da167024ac4b025ec223afc2caa1f0186287c763
PavelPoroskov <pavel.poroskov.se@gmail.com>
2024-05-17 23:37:11 +0200

    mv changes to Firefox

--- bookmark-info-firefox-mv2/background.js
+++ bookmark-info-firefox-mv2/background.js
@@ -343,9 +343,22 @@ async function getBookmarkInfoUni({ url, useCache=false }) {
     source,
   };
 }
-const cleanLink = (link) => {
+const exceptionList = [
+  'youtube.com',
+  'www.youtube.com',
+  'youtu.be',
+]
+const exceptionSet = new Set(exceptionList)
+
+const cleanLink = (link) => {
   try {
     const oLink = new URL(link);
+    const { hostname } = oLink;
+
+    if (exceptionSet.has(hostname)) {
+      return link
+    }
+
     oLink.search = ''
   
     return oLink.toString();  
@@ -354,8 +367,17 @@ async function getBookmarkInfoUni({ url, useCache=false }) {
   }
 }
 
-// const testStr = "https://www.linkedin.com/jobs/view/3920634940/?alternateChannel=search&refId=dvaqme%2FfxHehSAa5o4nVnA%3D%3D&trackingId=8%2FZKaGcTAInuTTH4NyKDoA%3D%3D"
-// console.log('test cleanLink', cleanLink(testStr))
+let testStr = "https://www.linkedin.com/jobs/view/3920634940/?alternateChannel=search&refId=dvaqme%2FfxHehSAa5o4nVnA%3D%3D&trackingId=8%2FZKaGcTAInuTTH4NyKDoA%3D%3D"
+console.log('test cleanLink', cleanLink(testStr))
+
+testStr = "https://www.youtube.com/watch?v=YuJ6SasIS_E&t=356s"
+console.log('test cleanLink', cleanLink(testStr))
+
+testStr = "https://youtube.com/watch?v=YuJ6SasIS_E&t=356s"
+console.log('test cleanLink', cleanLink(testStr))
+
+testStr = "https://youtu.be/watch?v=YuJ6SasIS_E&t=356s"
+console.log('test cleanLink', cleanLink(testStr))
 //
 // https://career.proxify.io/apply?uuid=566c933b-432e-64e0-b317-dd4390d6a74e&step=AdditionalInformation
 async function updateTabTask({ tabId, url, useCache=false }) {
