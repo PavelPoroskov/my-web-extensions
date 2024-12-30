7a2cd97392c5b378072fe8de8e631af553a99b8f
PavelPoroskov <pavel.poroskov.se@gmail.com>
2024-12-31 00:26:39 +0300

    change stracture of url settings

--- bookmark-info-chrome-mv3/api/url.api.config.js
+++ bookmark-info-chrome-mv3/api/url.api.config.js
@@ -7,49 +7,21 @@ import {
 
 const logUAC = makeLogFunction({ module: 'url.api.config' })
 
-// TODO-NEXT if we clean url on open then we loose check-in, check-out dates
-//    need search bookmark for clean url
-//    strategy01: clean url on open
-//    strategy02: clean url on save
-//    strategy00: don't clear url, default
-// {
-//   hostname: 'airbnb.com',
-//   paths: [
-//     '/rooms/',
-//   ]
-// },
-// {
-//   hostname: 'djinni.co',
-//   removeAllSearchParamForPath: [
-//     '/my/profile/',
-//     '/jobs/',
-//   ]
-// },
-
-// TODO the same
-// https://www.linkedin.com/jobs/collections/recommended/?currentJobId=4096094176
-// https://www.linkedin.com/jobs/view/4096094176/?alternateChannel=search&refId=WYo23okoaVDvmJkLoaclcg%3D%3D&trackingId=bTIytUCJN%2BDpKjaalPv4gg%3D%3D
-// https://www.linkedin.com/jobs/search/?currentJobId=4096094176&geoId=106686604&origin=JOBS_HOME_LOCATION_AUTOCOMPLETE&refresh=true
-
- const HOST_URL_SETTINGS = [
-  {
-    hostname: '9gag.com',
+const HOST_URL_SETTINGS = {
+  '9gag.com': {
     isHashRequired: true,
   },
-  {
-    hostname: 'forcoder.net',
+  'forcoder.net': {
     searchParamList: [
       's', // https://forcoder.net/?s=CQRS
     ],
   },
-  {
-    hostname: 'frontendmasters.com',
+  'frontendmasters.com': {
     removeAllSearchParamForPath: [
       '/courses/:id/',
     ]
   },
-  {
-    hostname: 'hh.ru',
+  'hh.ru': {
     removeAllSearchParamForPath: [
       '/vacancy/:id',
     ],
@@ -60,92 +32,66 @@ const logUAC = makeLogFunction({ module: 'url.api.config' })
       'professional_role',
     ],
   },
-  {
-    hostname: 'imdb.com',
-    // removeAllSearchParamForPath: [
-    //   '/title/',
-    //   '/list/',
-    //   '/imdbpicks/',
-    //   '/interest/',
-    //   '/',
-    // ],
-    //
+  'imdb.com': {
     searchParamList: [
       ['ref_'],
       'season', // https://www.imdb.com/title/tt8111088/episodes/?season=3&ref_=tt_eps_sn_3
     ],
   },
-  {
-    hostname: 'linkedin.com',
+  'linkedin.com': {
     removeAllSearchParamForPath: [
       '/jobs/view/:id/',
-      '/posts/:id/'
-    ]
+      '/posts/:id/',
+    ],
   },
-  {
-    hostname: 'opennet.ru',
+  'mail.google.com': {
+    isHashRequired: true,
+  },
+  'marketplace.visualstudio.com': {
+    searchParamList: [
+      'itemName',
+    ],
+  },
+  'opennet.ru': {
     searchParamList: [
       'num',
     ],
   },
-  {
-    hostname: 'thepiratebay.org',
+  'thepiratebay.org': {
     searchParamList: [
       'q',
       'id',
     ],
   },
-  {
-    hostname: 'torrentgalaxy.to',
+  'torrentgalaxy.to': {
     searchParamList: [
       'cat',
     ],
   },
-  {
-    hostname: 'udemy.com',
+  'udemy.com': {
     removeAllSearchParamForPath: [
       '/course/:id/',
-    ]
-  },
-  {
-    hostname: 'www.google.com',
-    searchParamList: [
-      'q', // https://www.google.com/search?q=react-native
     ],
   },
-  {
-    hostname: 'mail.google.com',
-    isHashRequired: true,
-  },
-  {
-    hostname: 'youtube.com',
-    hostnameAliasList: ['youtu.be'],
+  'www.google.com': {
     searchParamList: [
-      'v', // https://www.youtube.com/watch?v=qqqqq
+      'q',
     ],
   },
-  {
-    hostname: 'marketplace.visualstudio.com',
+  'youtu.be': 'youtube.com',
+  'youtube.com': {
     searchParamList: [
-      'itemName',
+      'v',
     ],
   },
-]
+}
 
-// all articles of author
-// https://dev.to/codewithsadee
-//  https://dev.to/:author
-//
-// article
-// https://dev.to/codewithsadee/build-deploy-a-stunning-saas-landing-page-with-react-tailwind-51p0
-//  https://dev.to/:author/:slug
-//
-// https://www.youtube.com/watch?v=qqqqq
-//  // https://www.youtube.com/watch?v=:slug
+function extendsSettings(oSettings) {
+  let mSettings = typeof oSettings == 'string'
+    ? HOST_URL_SETTINGS[oSettings]
+    : oSettings
 
-// logUAC('HOST_URL_SETTINGS', HOST_URL_SETTINGS.length, HOST_URL_SETTINGS)
-const HOST_URL_SETTINGS_LIST = HOST_URL_SETTINGS.map((item) => {
-  const searchParamList = item.searchParamList || []
+  const searchParamList = mSettings.searchParamList || []
   const importantSearchParamList = searchParamList
     .filter((searchParmName) => typeof searchParmName == 'string')
     .filter(Boolean)
@@ -156,16 +102,21 @@ const HOST_URL_SETTINGS_LIST = HOST_URL_SETTINGS.map((item) => {
     .filter(Boolean)
 
   return {
-    ...item,
+    ...mSettings,
     removeSearchParamList,
     importantSearchParamList,
+    isAlias: typeof oSettings == 'string',
   }
-})
+}
 
-// logUAC('HOST_URL_SETTINGS_LIST', HOST_URL_SETTINGS_LIST.length, HOST_URL_SETTINGS_LIST)
-const HOST_URL_SETTINGS_MAP = new Map(
-  HOST_URL_SETTINGS_LIST.map((item) => [item.hostname, item]),
-)
+const HOST_URL_SETTINGS_LIST = Object.entries(HOST_URL_SETTINGS)
+  .map(([hostname, oSettings]) => [
+    hostname,
+    extendsSettings(oSettings)
+  ])
+
+// logUAC('HOST_URL_SETTINGS', HOST_URL_SETTINGS)
+const HOST_URL_SETTINGS_MAP = new Map(HOST_URL_SETTINGS_LIST)
 
 export const getHostSettings = (url) => {
   logUAC('getHostSettings 00', url)
@@ -201,8 +152,8 @@ export const getHostSettings = (url) => {
 }
 
 export const HOST_LIST_FOR_PAGE_OPTIONS = HOST_URL_SETTINGS_LIST
-  .toSorted()
-  .filter(({ removeSearchParamList, removeAllSearchParamForPath }) => isNotEmptyArray(removeSearchParamList) || isNotEmptyArray(removeAllSearchParamForPath))
-  .map(
-    ({ hostname, removeAllSearchParamForPath }) => `${hostname}{${(removeAllSearchParamForPath || []).toSorted().join(',')}}`
-  )
+  .toSorted((a, b) => a[0].localeCompare(b[0]))
+  .filter(([, obj]) => (isNotEmptyArray(obj.removeSearchParamList) || isNotEmptyArray(obj.removeAllSearchParamForPath)) && !obj.isAlias)
+    .map(
+      ([hostname, obj]) => `${hostname}{${(obj.removeAllSearchParamForPath || []).toSorted().join(',')}}`
+    )
