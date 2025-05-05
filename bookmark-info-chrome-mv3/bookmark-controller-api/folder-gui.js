7522537d59f755007239e16bfb4e7f4a517d1cec
PavelPoroskov <pavel.poroskov.se@gmail.com>
2025-05-05 19:18:53 +0300

    refactor afterUserCreatedFolderInGUI()

--- bookmark-info-chrome-mv3/bookmark-controller-api/folder-gui.js
+++ bookmark-info-chrome-mv3/bookmark-controller-api/folder-gui.js
@@ -15,34 +15,33 @@ import {
 } from '../api-mid/index.js'
 
 export async function afterUserCreatedFolderInGUI({ id, parentId, title }) {
-  let correctParentId
-
+  const moveArgs = {}
   const settings = await extensionSettings.get()
 
   if (settings[USER_OPTION.USE_FLAT_FOLDER_STRUCTURE]) {
-    correctParentId = getNewFolderRootId(title)
+    const correctParentId = getNewFolderRootId(title)
+
+    if (parentId != correctParentId) {
+      moveArgs.parentId = correctParentId
+    }
   }
 
   const rootArray = [BOOKMARKS_BAR_FOLDER_ID, BOOKMARKS_MENU_FOLDER_ID, OTHER_BOOKMARKS_FOLDER_ID].filter(Boolean)
+  const finalParentId = moveArgs.parentId || parentId
 
-  const actualParentId = correctParentId || parentId
-  if (rootArray.includes(actualParentId)) {
-    const firstLevelNodeList = await chrome.bookmarks.getChildren(actualParentId)
+  if (rootArray.includes(finalParentId)) {
+    const firstLevelNodeList = await chrome.bookmarks.getChildren(finalParentId)
     const findIndex = firstLevelNodeList.find((item) => title.localeCompare(item.title) < 0)
 
-    const moveArgs = {}
-    if (correctParentId && parentId != correctParentId) {
-      moveArgs.parentId = correctParentId
-    }
     if (findIndex) {
       moveArgs.index = findIndex.index
     }
+  }
 
-    if (Object.keys(moveArgs).length > 0) {
-      moveFolderIgnoreInController({
-        id,
-        ...moveArgs,
-      })
-    }
+  if (0 < Object.keys(moveArgs).length) {
+    moveFolderIgnoreInController({
+      id,
+      ...moveArgs,
+    })
   }
 }
