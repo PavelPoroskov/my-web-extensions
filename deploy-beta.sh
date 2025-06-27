541ecb1b2acab349d1d1afb49bab3bd27c83a873
PavelPoroskov <pavel.poroskov.se@gmail.com>
2025-06-27 13:19:23 +0300

    deploy-beta for ff developer edition

---
+++ deploy-beta.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+VERSION=$(jq -r '.version' bookmark-info-chrome-mv3/manifest.json)
+WASERROR=
+
+node tools/mv-changes-to-firefox.mjs
+if [ $? -ne 0 ]; then
+    echo "Command failed! Move changes to Firefox"
+    exit 1
+fi
+echo Move changes to Firefox: Ok
+
+cd bookmark-info-firefox-mv2
+web-ext build --overwrite-dest
+if [ $? -ne 0 ]; then
+    echo "Command failed! web-ext build"
+    WASERROR=1
+fi
+cd - > /dev/null
+if [ -n "$WASERROR" ]; then
+    exit 1
+fi
+
+DISTRIBUTION_FF_FILE=bookmark_info-${VERSION}.zip
+DISTRIBUTION_FF_PATH_2=~/Documents/TMP-MANUAL/bookmark_info-${VERSION}b.zip
+cp bookmark-info-firefox-mv2/web-ext-artifacts/$DISTRIBUTION_FF_FILE $DISTRIBUTION_FF_PATH_2
+
+if [ $? -ne 0 ]; then
+    echo "Command failed! cp bookmark-info-firefox-mv2/web-ext-artifacts/$DISTRIBUTION_FF_FILE $DISTRIBUTION_FF_PATH"
+    exit 1
+fi
+
+echo SUCCESS
+echo Distribution for Firefox: $DISTRIBUTION_FF_PATH
